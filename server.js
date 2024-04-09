d53b6192e4927c2fd8a49119b6b130caa13f7d3b
punyaphatsura <punyaphat.sura@gmail.com>
2024-04-09 11:53:37 +0700

    feat: initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+app.get("/", (req, res) => {
+  res.status(200).json({ succes: true, data: { id: 1 } });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${process.env.NODE_ENV}`);
+});
