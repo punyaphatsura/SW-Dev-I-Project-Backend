612ed0b440c6255cfab252a4d6c253045d9b49e8
punyaphatsura <punyaphat.sura@gmail.com>
2024-04-09 11:55:49 +0700

    feat: initial commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 app.get("/", (req, res) => {
