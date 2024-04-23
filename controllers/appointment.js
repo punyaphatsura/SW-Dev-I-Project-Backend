8cbf217e50c9369ada7bbecb0100b86cdc026f3d
JinnySarita <6432181821@student.chula.ac.th>
2024-04-24 05:18:15 +0700

    feat: :sparkles: add controller appointment

---
+++ controllers/appointment.js
@@ -0,0 +1,169 @@
+import Restaurant from "../models/Restaurant.js";
+import { AppointmentSchema as Appointment } from "../models/Appointment";
+
+export const getAppointments = async (req, res, next) => {
+  let query;
+
+  if (req.user.role !== "admin") {
+    query = Appointment.find({ user: req.user._id }).populate({
+      path: "appointment",
+      select: "name province tel opentime closetime",
+    });
+  } else {
+    if (req.params.restaurantId) {
+      query = Appointment.find({ restaurant: req.params.restaurantId }).populate({
+        path: "restaurant",
+        select: "name province tel opentime closetime",
+      });
+    } else {
+      query = Appointment.find().populate({
+        path: "restaurant",
+        select: "name province tel opentime closetime",
+      });
+    }
+  }
+  try {
+    const appointments = await query;
+    res.status(200).json({
+      success: true,
+      count: appointments.length,
+      data: appointments,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Cannot find Appointment",
+    });
+  }
+};
+
+export const getAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.findById(req.params.id).populate({
+      path: "restaurant",
+      select: "name description tel opentime closetime",
+    });
+
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id ${req.params.id}`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Cannot find Appointment",
+    });
+  }
+};
+
+export const addAppointment = async (req, res, next) => {
+  try {
+    req.body.restaurant = req.params.restaurantId;
+
+    const restaurant = await Restaurant.findById(req.params.restaurantId);
+
+    if (!restaurant) {
+      return res.status(404).json({
+        success: false,
+        message: `No restaurant with the id of ${req.params.restaurantId}`,
+      });
+    }
+
+    req.body.user = req.user._id;
+
+    const existedAppointments = await Appointment.find({ user: req.user.id });
+
+    if (existedAppointments.length >= 3 && req.user.role !== 'admin') {
+      return res.status(400).json({
+        success: false,
+        message: `The user with ID ${req.user.id} has already made 3 appointments.`,
+      });
+    }
+
+    const appointment = await Appointment.create(req.body);
+    return res.status(201).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (error) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot create Appointment",
+    });
+  }
+};
+
+export const updateAppointment = async (req, res, next) => {
+  try {
+    let appointment = await Appointment.findById(req.params.id);
+
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id of ${req.params.id}`,
+      });
+    }
+
+    if (appointment.user.toString() !== req.user.id && req.user.role !== 'admin') {
+      return res.status(401).json({
+        success: false,
+        message: `User ${req.user.id} is not authorized to update this appointment.`,
+      });
+    }
+    appointment = await Appointment.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    return res.status(200).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (error) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot update Appointment",
+    });
+  }
+};
+
+export const deleteAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.findById(req.params.id);
+
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id of ${req.params.id}`,
+      });
+    }
+
+    if (appointment.user.toString() !== req.user.id && req.user.role !== 'admin') {
+      return res.status(401).json({
+        success: false,
+        message: `User ${req.user.id} is not authorized to delete this appointment.`,
+      });
+    }
+
+    await appointment.deleteOne();
+    return res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (error) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot delete Appointment",
+    });
+  }
+};
--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -102,7 +102,7 @@ export const createRestaurant = async (req, res, next) => {
   try {
     console.log(req.body);
     const restaurant = await Restaurant.create(req.body);
-    res.status(200).json({
+    res.status(201).json({
       success: true,
       data: restaurant,
     });
